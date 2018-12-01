3aceb96fb64e8a2927a9f8364d8981db50925906
hhhhh <unai.tapia.muriel@gmail.com>
2018-12-01 17:43:14 +0100

    Puntos de inicio - eventos al tocar el agua

---
+++ kidyTechy_v2/Assets/scripts/islandScene/OnTouchWaterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTouchWaterScript : MonoBehaviour {
+	public GameObject startPoint;
+	public GameObject point1;
+	public GameObject point2;
+	public GameObject point3;
+	public GameObject boyObj;
+	public int islandIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+		if(islandIndex == 0) {
+            boyObj.transform.position = startPoint.transform.position;
+
+		} else if(islandIndex == 1){
+			boyObj.transform.position = point1.transform.position;
+
+		} else if(islandIndex == 2){
+			boyObj.transform.position = point2.transform.position;
+
+		} else if(islandIndex== 3){
+            boyObj.transform.position = point3.transform.position;
+		}
+       
+    }
+}
